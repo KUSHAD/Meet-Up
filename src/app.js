b73f9f374e21b3059b6d01fa735f9964df574702
KUSHAD <kushadck@gmail.com>
2020-08-28 12:31:44 +0530

    updated editorconfig

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(favicon(path.join(__dirname, 'favicon.ico')));
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/index.html');
 });
 
 io.of('/stream').on('connection', stream);
